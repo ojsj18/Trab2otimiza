bd64c4457a8d6da66fc3a621ad2fec4e04a9450c
ojsj18 <juhnior88@gmail.com>
2021-11-27 11:41:31 -0300

    terminado e corrigido com os exemplos

--- otimização.py
+++ otimização.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python2
 import sys
 
 def acha_o_maior(mochila,j):
@@ -9,14 +10,12 @@ def acha_o_maior(mochila,j):
     maior = ordenado1[j][1]
     return maior
 
-def maximador(lista):
+def maximizador(lista):
     #recebe a lista e calcula a função de maximização, acho que usar a global vai dar boa
     global pesos
-    print(lista)
     soma=0
     for i in range (0,len(lista)):
         soma = soma + pesos[i]*lista[i]
-    print("maximo ",soma) 
     return soma
 
 def achaproibido(item):
@@ -28,12 +27,11 @@ def achaproibido(item):
 
 def itensmochila(item,mochila,lista):
     global c
-    global listinha
+    global lista_escolhidos
     global valores_itens
     n = 0
     if item == -1:
         return False
-
     for i in range(0,len(mochila)):
         if item == mochila[i]:
             return True
@@ -41,7 +39,7 @@ def itensmochila(item,mochila,lista):
             return True
         if valores_itens[item-1]>c:
             return True 
-        if (listinha[mochila[i]-1][2] == item) or (mochila[i] == listinha[item-1][2]):
+        if (lista_escolhidos[mochila[i]-1][2] == item) or (mochila[i] == lista_escolhidos[item-1][2]):
             return True
     return False
 
@@ -60,12 +58,10 @@ def create_mochila(lista):
     for i in range(0,len(lista)):
         if lista[i] == 2:
             if(itensmochila(i+1,mochila,lista)):
-                print("inviavel")
                 return -1
             mochila.append(i+1)
             j = j+ valores_itens[i]
     if j > c:
-        print("inviavel")
         return-1
         
     return mochila
@@ -78,33 +74,34 @@ def create_results(mochila):
 
     return result
 
-def branch_create(lista,maior):
+def branch_create(lista,maior,z):
+    z=z+1
+
     mochila = create_mochila(lista)
     if mochila == -1:
         return -1
     i = tamanho_mochila(mochila)
-    print(i)
-    global valores_itens
-    global c,n,max1
+    result = create_results(mochila)
+    
+    global valores_itens,limitador
+    global c,n
     
     j = 0
     item = 0
     branch = False
-    result = create_results(mochila)
+    maximo = list()
     
     while True:
         item = acha_o_maior(mochila,j)
         j = j+1
-
-        while(itensmochila(item,mochila,lista) or valores_itens[item-1]>c):
+        while(itensmochila(item,mochila,lista) or valores_itens[item-1]>c and item!= -1):
             item = acha_o_maior(mochila,j)
             j=j+1
         if item == -1:
-            break
+                break
         if i==c:
             break
         if i+valores_itens[item-1]>c:
-            print(i+valores_itens[item-1])
             branch = True
             branch_fix1 = list(lista)
             branch_fix1[item-1]= 2
@@ -118,42 +115,51 @@ def branch_create(lista,maior):
             i = i+valores_itens[item-1]
             result[item-1]=1
             
-    maximo= maximador(result)
+    maximo.append(maximizador(result))
+    maximo.append(mochila)
+
     if(i==c):
-        print("integralidade")
+        if limitador[0] < maximo[0]:
+           limitador = list(maximo)
+        return limitador
+    if maximo[0] <= limitador[0] and not branch:
+        return limitador
+    if(item == -1):
+        if limitador[0] < maximo[0]:
+           limitador = list(maximo)
         return maximo
-    if maximo <=maior  and not branch:
-        print("menor")
-        return maior
     if(branch):
-       print("ramifica")
-       a = branch_create(branch_fix1,maximo)
-       b = branch_create(branch_fix0,maximo)
+       if limitador[0]>= maximo[0]:
+           return limitador
+       a = branch_create(branch_fix1,maximo,z)
+       b = branch_create(branch_fix0,maximo,z)
        if a>b:
            return a
        else:
-            return b
-    if(item == -1):
-        print("integralidade")
-        return maximo
-
+           return b
 
 # tratamento de entradas
 n,m,c = map(int, sys.stdin.readline().split())
 valores_itens = map(int, sys.stdin.readline().split())
 pesos = map(int, sys.stdin.readline().split())
 duplas = map(int, sys.stdin.read().split())
-
-#ordenando pelo valor relativo
-
-listinha = list()
+lista_escolhidos = list()
+limitador = [0]
 item = [0]*3
+
 for i in range(0,n):
     item[0]= pesos[i]/float(valores_itens[i])
     item[1]= i+1
     item[2]= achaproibido(i+1)
-    listinha.append(item[:])
+    lista_escolhidos.append(item[:])
 
-ordenado1 = sorted(listinha, key= lambda item : item[0],reverse=True)
-maximo = branch_create([0]*n,0)
-print("resposta",maximo)
+ordenado1 = sorted(lista_escolhidos, key= lambda item : item[0],reverse=True)
+bolsa_final = branch_create([0]*n,0,0)
+print(bolsa_final[0])
+if bolsa_final[0]>0:
+    bolsa_final[1] = sorted(bolsa_final[1])
+
+saida= str()
+for itens in bolsa_final[1]:
+    saida = saida+str(itens)+" "
+print(saida)
